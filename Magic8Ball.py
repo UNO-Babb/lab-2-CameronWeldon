e8c634ffc344d0288ee12094930578b1c5b0132c
CameronWeldon <cameronweldon@unomaha.edu>
2025-02-03 03:21:23 +0000

    Done Lab 2.

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -8,11 +8,21 @@ import random
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+  print("Generate Random Number and Month")
   #Prompt the user for their question.
-
+  answers = ["January", "February", "March", "April", 
+              "May", "June", "July", "August",
+              "September", "October", "November", "December"]
   #Answer question randomly with one of the options from your earlier list.
-
-
+  length = len(answers)
+  r = random.random() * 12
+  
+  r = int(r)
+  
+  
+  print(r)
+  response = answers[r]
+  print(response)
+  
 if __name__ == '__main__':
   main()
